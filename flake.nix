615d19fb4568c413fe5fca5c471abcd7bce6f805
timothy <git@timothyklim.com>
2023-03-10 21:00:51 +0700

    chore: upgrade jdk repo

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
       flake = false;
     };
     yourkit_zip = {
-      url = "https://download.yourkit.com/yjp/2022.9/YourKit-JavaProfiler-2022.9-b177.zip";
+      url = "https://download.yourkit.com/yjp/2022.9/YourKit-JavaProfiler-2022.9-b179.zip";
       flake = false;
     };
 
