6483b458843f706aa4ca810e34dedcae905fbbd6
timothy <git@timothyklim.com>
2023-12-04 06:18:16 +0700

    fix(jextract): jdk21_home

--- build/jextract.nix
+++ build/jextract.nix
@@ -4,7 +4,7 @@ with pkgs;
 
 let
   llvm_home = llvmPackages_15.libclang.lib;
-  jdkPrefix = if openjdk_22 != null then "-Pjdk22_home=${openjdk_22.home}" else "-Pjdk20_home=${openjdk_21.home}";
+  jdkPrefix = if openjdk_22 != null then "-Pjdk22_home=${openjdk_22.home}" else "-Pjdk21_home=${openjdk_21.home}";
   buildGradleCmd = cmd: "gradle --no-daemon ${jdkPrefix} -Pllvm_home=${llvm_home} ${cmd}";
   makePackage = args: stdenv.mkDerivation ({
     inherit src;
