fcbd582e6329233d1089ab5fe1eb9338bb453f4c
Timothy Klim <git@timothyklim.com>
2024-09-11 14:17:00 +0700

    fix: jextract for macOS

--- build/jextract.nix
+++ build/jextract.nix
@@ -9,6 +9,7 @@ with pkgs; stdenv.mkDerivation {
   dontUseCmakeConfigure = true;
 
   buildPhase = ''
+    export GRADLE_USER_HOME=$(mktemp -d)
     gradle --no-daemon -Pjdk22_home=${jdk_22.home} -Pllvm_home=${llvmPackages_13.libclang.lib} build
   '';
 
@@ -18,7 +19,7 @@ with pkgs; stdenv.mkDerivation {
 
     sed -e 's;ROOT=`dirname $0`;ROOT=`dirname $(readlink -f -- $0)`;g' \
       -i $out/bin/jextract
-
+  '' + lib.optionalString stdenv.isLinux ''
     patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
       --set-rpath "${lib.makeLibraryPath [ zlib ]}:$out/runtime/lib" $out/runtime/bin/java
   '';
