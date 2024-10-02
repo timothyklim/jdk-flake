e0c5d537c82c0809314deb1330743ae16896fc19
Dmitry Zhevlov <d.zhevlov@tawasal.ae>
2024-10-02 16:36:52 +0300

    Fix build jattach on macos aarm64

--- build/jattach.nix
+++ build/jattach.nix
@@ -4,6 +4,11 @@ with pkgs; stdenv.mkDerivation rec {
   inherit src;
   name = "jattach";
 
+  preBuild = (lib.optionalString stdenv.isDarwin ''
+    substituteInPlace Makefile \
+      --replace "CFLAGS ?= -O3 -arch x86_64 -arch arm64 -mmacos-version-min=10.12" "CFLAGS ?= -O3 -arch arm64 -mmacos-version-min=10.12"
+  '');
+
   installPhase = ''
     mkdir -p $out/bin
     cp -r build/jattach $out/bin/
