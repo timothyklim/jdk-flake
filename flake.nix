b98025cc1de3f229d3442efb441e6cbfb4a8a095
timothy <git@timothyklim.com>
2025-02-20 08:20:57 +0700

    feat: add static JDK 24-ea builds

--- flake.nix
+++ flake.nix
@@ -81,6 +81,15 @@
       flake = false;
     };
 
+    zulu24_linux_x64_tgz = {
+      url = "https://cdn.azul.com/zulu/bin/zulu24.0.77-beta-jdk24.0.0-beta.34-linux_x64.tar.gz";
+      flake = false;
+    };
+    zulu24_macos_aarch64_tgz = {
+      url = "https://cdn.azul.com/zulu/bin/zulu24.0.77-beta-jdk24.0.0-beta.34-macosx_aarch64.zip";
+      flake = false;
+    };
+
     # Zing
     # zing21_linux_tgz = {
     #   url = "https://ftp.azul.com/releases/Zing/ZVM24.02.0.0/zing24.02.0.0-6-jdk21.0.2-linux_x64.tar.gz";
@@ -109,6 +118,8 @@
       # , yourkit_zip
     , zulu23_linux_x64_tgz
     , zulu23_macos_aarch64_tgz
+    , zulu24_linux_x64_tgz
+    , zulu24_macos_aarch64_tgz
       # , zing21_linux_tgz
     }:
       with flake-utils.lib; with system; eachSystem [ x86_64-linux aarch64-linux aarch64-darwin ] (system:
@@ -258,6 +269,17 @@
           version = "23.0.1";
         };
 
+        zulu_24_linux = import ./build/zulu.nix {
+          inherit pkgs;
+          src = zulu24_linux_x64_tgz;
+          version = "24-ea+34";
+        };
+        zulu_24_macos = import ./build/zulu.nix {
+          inherit pkgs;
+          src = zulu24_macos_aarch64_tgz;
+          version = "24-ea+34";
+        };
+
         # zing_21 = import ./build/zing.nix {
         #   inherit pkgs;
         #   src = zing17_linux_tgz;
@@ -266,6 +288,7 @@
 
         jdk_21 = if pkgs.stdenv.isLinux then openjdk_21 else pkgs.zulu21;
         jdk_23 = if pkgs.stdenv.isLinux then zulu_23_linux else zulu_23_macos;
+        jdk_24 = if pkgs.stdenv.isLinux then zulu_24_linux else zulu_24_macos;
 
         jdk = openjdk_23;
 
@@ -278,7 +301,7 @@
             jtreg jextract jmc jitwatch visualvm
             async-profiler jattach
             # jprofiler yourkit
-            jdk_21 jdk_23
+            jdk_21 jdk_23 jdk_24
             # zing_21
             ;
         };
